5ed4ccaec174a068ff36937ddba5e96f86c582ac
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-09-22 05:38:23 +0600

    Handling user in middleware, logout, and redirection

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -1,9 +1,9 @@
 "use server";
 
 import axiosInstance from "@/src/lib/AxiosInstance";
-import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
+import { jwtDecode } from "jwt-decode";
 
 export const registerUser = async (userData: FieldValues) => {
   try {
@@ -22,7 +22,7 @@ export const registerUser = async (userData: FieldValues) => {
 
 export const loginUser = async (userData: FieldValues) => {
   try {
-    const { data } = await axiosInstance.post("/auth/register", userData);
+    const { data } = await axiosInstance.post("/auth/login", userData);
 
     if (data.success) {
       cookies().set("accessToken", data?.data?.accessToken);
@@ -35,6 +35,10 @@ export const loginUser = async (userData: FieldValues) => {
   }
 };
 
+export const logout = () => {
+  cookies().delete("accessToken");
+  cookies().delete("refreshToken");
+};
 
 export const getCurrentUser = async () => {
   const accessToken = cookies().get("accessToken")?.value;
